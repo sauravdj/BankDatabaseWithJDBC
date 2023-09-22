a1f7c934fc5c4386370e986d2500e4797e19b61f
Saurav Jadhav <sauravdjadhav@gmail.com>
2023-09-22 20:17:30 +0530

    adding java file for various DataBase actions

---
+++ AccOperations.java
@@ -0,0 +1,144 @@
+package BankDatabaseWithJDBC;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class AccOperations {
+
+	public static boolean createAcc(BankAcc ba){
+		boolean flag = false;
+		try {
+			Connection conn = Connector.createConn();
+			String q="insert into BankAccount(custName,balance) values (?,?)";
+			PreparedStatement pstmt = conn.prepareStatement(q);
+			pstmt.setString(1, ba.getCustName());
+			pstmt.setDouble(2, ba.getBalance());
+			pstmt.executeUpdate();
+			flag = true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static boolean showAllAcc(BankAcc ba) {
+		boolean flag = false;
+		try{
+			Connection conn = Connector.createConn();
+			String q="select * from BankAccount";
+			Statement stmt = conn.createStatement();
+			ResultSet set = stmt.executeQuery(q);
+			while(set.next()){
+				int accNo = set.getInt(1);
+				String name = set.getString("custName");
+				Double balance = set.getDouble("balance");
+				System.out.println("Account no: "+accNo);
+				System.out.println("Account holder name: "+name);
+				System.out.println("Account Balance: "+balance);
+				System.out.println("=======================");
+			}
+			flag = true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static boolean deleteAcc(BankAcc ba){
+		boolean flag = false;
+		try {
+			Connection conn = Connector.createConn();
+			String q="delete from BankAccount where accNo=?";
+			PreparedStatement pstmt = conn.prepareStatement(q);
+			pstmt.setLong(1,ba.getAccNo());
+			
+		    pstmt.executeUpdate();
+			flag = true;
+		} catch (Exception e){
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static boolean showAcc(BankAcc ba){
+		boolean flag = false;
+		try {
+			Connection conn = Connector.createConn();
+			String q="select * from BankAccount where accNo = ? ";
+			PreparedStatement pstmt = conn.prepareStatement(q);
+			pstmt.setLong(1,ba.getAccNo());
+			ResultSet set = pstmt.executeQuery();
+			while(set.next()){
+				int accNo = set.getInt("accNo");
+				String name = set.getString("custName");
+				Double balance = set.getDouble("balance");
+				System.out.println("Account no: "+accNo);
+				System.out.println("Account holder name: "+name);
+				System.out.println("Account Balance: "+balance);
+				
+			}
+			flag = true;
+		}catch (Exception e){
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static boolean depositeBalance(BankAcc ba, double amount){
+		boolean flag = false;
+		try{
+			Connection conn = Connector.createConn();
+			String q="select * from BankAccount where accNo = ? ";
+			PreparedStatement pstmt = conn.prepareStatement(q);
+			pstmt.setLong(1,ba.getAccNo());
+			
+			ResultSet set = pstmt.executeQuery();
+			set.next();
+			
+			double balance = set.getDouble("balance");
+			double newBalance = balance + amount;
+				
+			String q1 = "update BankAccount set balance = ? where accNo = ?";	
+			PreparedStatement pstmt1 = conn.prepareStatement(q1);
+			pstmt1.setDouble(1,newBalance);
+			pstmt1.setLong(2,ba.getAccNo());
+			pstmt1.executeUpdate();
+			flag = true;
+            
+		}catch (Exception e){
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static boolean withdrawBalance(BankAcc ba, double amount){
+		boolean flag = false;
+		try{
+			Connection conn = Connector.createConn();
+			String q="select * from BankAccount where accNo = ?";
+			PreparedStatement pstmt = conn.prepareStatement(q);
+			pstmt.setLong(1,ba.getAccNo());
+			
+			ResultSet set = pstmt.executeQuery();
+			set.next();
+			
+			double balance = set.getDouble("balance");
+			if(amount < balance){
+				double newBalance = balance - amount;
+				String q1 = "update BankAccount set balance = ? where accNo = ?";	
+				PreparedStatement pstmt1 = conn.prepareStatement(q1);
+				pstmt1.setDouble(1, newBalance);
+				pstmt1.setLong(2, ba.getAccNo());
+				pstmt1.executeUpdate();
+				flag = true;
+			}
+			else
+				System.out.println("Insufficient balance can't be withdraw....");
+			
+		} catch (Exception e){
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return flag;
+	}
+}
